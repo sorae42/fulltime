d41b8f456dd1db94137a294509288e42a6070c84
Rosetta Emerson <bonniefoxy2009@gmail.com>
2021-10-07 13:46:46 +0700

    initial commit

---
+++ fulltime.c
@@ -0,0 +1,43 @@
+/*
+ * fulltime - Tell the time in full-formatted text
+ *
+ * Author: Rosetta / skyventuree / sora
+ *
+ */ 
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+	const char numbers[12][7] = {"twelve", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"};
+
+	const char extras[10][10] = {"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+	time_t now = time(NULL);
+	struct tm *tm_struct = localtime(&now);
+	int hour = tm_struct->tm_hour;
+	if (hour >= 12) hour = hour - 12;
+	int minute = tm_struct->tm_min;
+
+	// it looks like a spagetti of if else statement
+	// any improvement to code reading of this part is appreciated
+	if( minute == 0 ) printf("%s o' clock\n", numbers[hour]);
+	else if ( minute == 15 ) printf("quarter past %s\n", numbers[hour]);
+	else if ( minute == 30 ) printf("half past %s\n", numbers[hour]);
+	else if ( minute == 45 ) printf("quarter to  %s\n", numbers[hour]);
+	else if ( minute < 30 ) {
+		if ( minute <= 10 ) printf("%s past %s\n", numbers[minute], numbers[hour]);
+		else if (minute < 20 ) printf("%s past %s\n", extras[minute-10],numbers[hour]);
+		else if (minute == 20 ) printf("twenty past %s\n", numbers[hour]);
+		else printf("twenty-%s past %s\n", numbers[minute-20], numbers[hour]);
+	}
+	else {
+		if ( minute <= 40 ) printf("twenty-%s to %s\n", numbers[10-(minute-30)], numbers[hour]);
+		else if (minute == 40 ) printf("twenty to %s\n", numbers[hour]);
+		else if (minute < 50 ) printf("twenty-%s to %s\n", numbers[10-(minute-40)], numbers[hour]);
+		else printf("%s to %s\n", numbers[10-(minute-50)], numbers[hour]);
+	}
+
+	return 0;
+}
